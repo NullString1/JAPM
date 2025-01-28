6067d86b16432b6d92f096c78d7c9fefae04f393
NullString <53035336+nullstring1@users.noreply.github.com>
2025-01-28 17:16:21 +0000

    Add data loaded toast

--- japm.js
+++ japm.js
@@ -320,6 +320,7 @@ class JAPM {
             });
             $("#load-input").change(() => {
                 $("#login-submit").text("Login");
+                bootstrap.Toast.getOrCreateInstance($("#data-loaded-toast")[0]).show();
             });
             $("#reset-data-button").click(() => {
                 localStorage.removeItem("japm");
